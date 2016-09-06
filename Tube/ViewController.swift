6bd2517b326d8027046610edbd52bfbe95af98d3
bokafor900 <okafor.bryan@yahoo.com>
2016-09-06 01:08:28 -0500

    Initial Commit

---
+++ Tube/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tube
+//
+//  Created by Bryan Okafor on 9/6/16.
+//  Copyright © 2016 APEX Dominion Industries. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
